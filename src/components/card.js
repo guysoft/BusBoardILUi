3afd9c0804baf00aba6b79fe09c1070c346d8586
Pavel Shermayster <sherm84@gmail.com>
2018-11-30 16:55:36 +0200

    feat: added simple styling

---
+++ src/components/card.js
@@ -0,0 +1,49 @@
+export class AppCard extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  set data(data) {
+    console.log("data", data);
+    this.shadowRoot.innerHTML = render(data);
+  }
+  disconnectedCallback() {}
+}
+
+AppCard.observedAttributes = ["greet", "data"];
+
+function render(data) {
+  return `
+    ${getStyles()}
+    <div class="card">
+      <div class="line-name">קו: ${data.line_name}</div>
+      <div class="street">רחוב: ${data.stop_street}</div>
+      <div class="estimation">הגעה: ${getEstimation(data.eta)}</div>
+      <div class="destination">יעד: ${
+        data.static_info.route.destination.name.HE
+      }</div>
+    </div>
+    `;
+}
+
+function getStyles() {
+  return `
+  <style>
+    .card {
+      display: flex;
+      flex-direction: column;
+      text-align: right;
+      padding: 1rem;
+      background-clip: border-box;
+      box-shadow: rgba(0, 0, 0, 0.14) 0px 2px 2px;
+    }
+  </style>
+  `;
+}
+
+function getEstimation(time) {
+  const result = Date.parse(time) - Date.now();
+  const minutes = Math.round(((result % 86400000) % 3600000) / 60000);
+  return minutes;
+}
--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
-import { AppCard } from "./card";
+import { AppCard } from "./components/card";
 import UpdatesService from "./updatesService";
 import response from "./mock";
 
 window.customElements.define("app-card", AppCard);
 var body = document.querySelector("body");
+var container = document.createElement("div");
+container.setAttribute("class", "container");
+body.appendChild(container);
 
-const updatesService = new UpdatesService();
+// const updatesService = new UpdatesService();
 
-updatesService.getBuses("32.0744198", "34.795655200000056").then(stops => {
+// updatesService.getBuses("32.0744198", "34.795655200000056").then(stops => {
+//   createBusCards(stops);
+// });
+
+createBusCards(response);
+
+function createBusCards(stops) {
   Object.keys(stops).forEach(stopName => {
-    bus = stops[stopName].forEach(busData => {
+    stops[stopName].forEach(busData => {
       addBusCard(busData);
+      console.log(busData);
     });
   });
-});
+}
 
 function addBusCard(busData) {
   var card = document.createElement("app-card");
-  card.setAttribute("data", JSON.stringify(busData));
-  body.appendChild(card);
+  card.data = busData;
+  container.appendChild(card);
 }
